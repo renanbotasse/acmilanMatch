55a3a860b4de9bc887656f786a9616bf04568eef
renanbotasse <renanbotasse@gmail.com>
2023-02-28 14:10:24 +0000

    timezone fixed

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("73fe1a77e453b80406a2b15408e1b538e61c65c0d381f644bbb53feca5208643");
-const local = Intl.DateTimeFormat().resolvedOptions().timeZone;
-const [fakeLocal, realLocal] = local.split('/');
 const params = {
   q: "ac milan",
-  location: realLocal
+  location: "italy"
 };
 
 const getGameData = function(callback) {
@@ -16,7 +14,7 @@ const getGameData = function(callback) {
     const tournament = data.sports_results.games[0].tournament;
     const date = [data.sports_results.games[0].date, + ' ' + data.sports_results.games[0].time];
     const gameData = { homeTeam, homeTeamImgLink, awayTeam, awayTeamImgLink, tournament, date };
-    
+  
     callback(gameData);
   });
 }
